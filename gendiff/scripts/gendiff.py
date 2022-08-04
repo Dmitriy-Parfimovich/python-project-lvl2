8168a8cbac244bf9de6e355ecb5620e1a85581b7
Dmitriy Parfimovich <dparf@mail.ru>
2022-08-04 22:00:38 +0300

    step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,6 +10,7 @@ def main():
 
     parser.add_argument("first_file", help = 'first file to compare')
     parser.add_argument("second_file", help = 'second file to compare')
+    parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
 
