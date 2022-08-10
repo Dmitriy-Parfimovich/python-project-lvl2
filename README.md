2de7fa4742592e4a6d46d4d8ed428790c2e90cd7
Dmitriy Parfimovich <dparf@mail.ru>
2022-08-10 16:30:55 +0300

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Dmitriy-Parfimovich/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Dmitriy-Parfimovich/python-project-lvl2/actions)
+
+
+## _The generate_diff module work:_
+[![asciicast](https://asciinema.org/a/nO4uGqeFTexRbQfpKkLBTSdbQ.svg)](https://asciinema.org/a/nO4uGqeFTexRbQfpKkLBTSdbQ)
---
+++ gendiff/modules/generate_diff.py
@@ -0,0 +1,47 @@
+import json
+from collections import OrderedDict
+
+
+def generate_diff(file_path1, file_path2):
+    with open(file_path1, 'r', encoding='utf-8') as f1:
+        text1 = json.load(f1)
+    with open(file_path2, 'r', encoding='utf-8') as f2:
+        text2 = json.load(f2)
+    key_list, value_list, result = [], [], []
+    for key1 in text1.keys():
+        for key2 in text2.keys():
+            if key1 == key2 and text1[key1] == text2[key2]:
+                key_list.append(key1)
+                value_list.append(text1[key1])
+            if key1 == key2 and text1[key1] != text2[key2]:
+                key_list.append(f'{key1}-X_*')
+                key_list.append(f'{key2}-Y_*')
+                value_list.append(text1[key1])
+                value_list.append(text2[key2])
+            if key1 not in text2.keys():
+                key_list.append(f'{key1}-X_*')
+                value_list.append(text1[key1])
+            if key2 not in text1.keys():
+                key_list.append(f'{key2}-Y_*')
+                value_list.append(text2[key2])
+    result = dict(zip(key_list, value_list)).items()
+    result = dict(sorted(result))
+    ordered_result = OrderedDict(result.items())
+    ordered_result_right = OrderedDict()
+    for key in ordered_result:
+        key_list = list(key)
+        if key_list[-4::] != ['-', 'X', '_', '*'] and key_list[-4::] != ['-', 'Y', '_', '*']:
+            ordered_result_right['  ' + key] = ordered_result[key]
+        if key_list[-4::] == ['-', 'X', '_', '*']:
+            key_list = key_list[0:-4]
+            new_key = ['- ' + ''.join(key_list)]
+            ordered_result_right[new_key[0]] = ordered_result[key]
+        if key_list[-4::] == ['-', 'Y', '_', '*']:
+            key_list = key_list[0:-4]
+            new_key = ['+ ' + ''.join(key_list)]
+            ordered_result_right[new_key[0]] = ordered_result[key]
+    print('{')
+    for key, value in ordered_result_right.items():
+        print(f'  {key}: {str(value).lower()}')
+    print('}')
+
