41e36c2da47268a294fa4dd7e4f919f606c2b9f0
Dmitriy Parfimovich <dparf@mail.ru>
2022-08-12 14:44:41 +0300

    step 4 fix flake8 noqa

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,1 +1,2 @@
+# flake8: noqa
 from gendiff.modules.generate_diff import generate_diff
--- gendiff/modules/generate_diff.py
+++ gendiff/modules/generate_diff.py
@@ -1,6 +1,30 @@
 import json
 from collections import OrderedDict
 
+# flake8: noqa
+def get_diff_dict(text1, text2):
+    key_list, value_list, result = [], [], []
+    for key1 in text1.keys():
+        for key2 in text2.keys():
+            if key1 == key2 and text1[key1] == text2[key2]:
+                key_list.append(key1)
+                value_list.append(text1[key1])
+            if key1 == key2 and text1[key1] != text2[key2]:
+                key_list.append(f'{key1}-X_*')
+                key_list.append(f'{key2}-Y_*')
+                value_list.append(text1[key1])
+                value_list.append(text2[key2])
+            if key1 not in text2.keys():
+                key_list.append(f'{key1}-X_*')
+                value_list.append(text1[key1])
+            if key2 not in text1.keys():
+                key_list.append(f'{key2}-Y_*')
+                value_list.append(text2[key2])
+    result = dict(zip(key_list, value_list)).items()
+    result = dict(sorted(result))
+    ordered_result = OrderedDict(result.items())
+    return ordered_result
+
 
 def get_correct_output(ordered_result):
     ordered_result_right = OrderedDict()
@@ -28,24 +52,5 @@ def generate_diff(file_path1, file_path2):
         text1 = json.load(f1)
     with open(file_path2, 'r', encoding='utf-8') as f2:
         text2 = json.load(f2)
-    key_list, value_list, result = [], [], []
-    for key1 in text1.keys():
-        for key2 in text2.keys():
-            if key1 == key2 and text1[key1] == text2[key2]:
-                key_list.append(key1)
-                value_list.append(text1[key1])
-            if key1 == key2 and text1[key1] != text2[key2]:
-                key_list.append(f'{key1}-X_*')
-                key_list.append(f'{key2}-Y_*')
-                value_list.append(text1[key1])
-                value_list.append(text2[key2])
-            if key1 not in text2.keys():
-                key_list.append(f'{key1}-X_*')
-                value_list.append(text1[key1])
-            if key2 not in text1.keys():
-                key_list.append(f'{key2}-Y_*')
-                value_list.append(text2[key2])
-    result = dict(zip(key_list, value_list)).items()
-    result = dict(sorted(result))
-    ordered_result = OrderedDict(result.items())
+    ordered_result = get_diff_dict(text1, text2)
     get_correct_output(ordered_result)
