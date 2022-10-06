f2429ce8687b05009a13c5fb223ff79bb9d1b92b
Dmitriy Parfimovich <dparf@mail.ru>
2022-10-06 21:17:08 +0300

    step 6 fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,7 +11,8 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration\
     files and shows a difference.')
 
-    parser.add_argument('-f', '--format', default=stylish, help='set format of output')
+    parser.add_argument('-f', '--format', default=stylish,
+                        help='set format of output')
     parser.add_argument('first_file', help='first file to compare')
     parser.add_argument('second_file', help='second file to compare')
 
