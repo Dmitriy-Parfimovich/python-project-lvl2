b0e29f08190d16cf7616aeb3f41e7794f035123f
Dmitriy Parfimovich <dparf@mail.ru>
2022-12-10 15:54:34 +0300

    fix check 2_2

--- gendiff/plain.py
+++ gendiff/plain.py
@@ -10,6 +10,7 @@ ADDED = 'added'
 REMOVED = 'removed'
 CHANGED = 'changed'
 NESTED = 'nested'
+COMPLEX = '[complex value]'
 
 
 def get_plain_list(tree): # noqa
@@ -28,40 +29,45 @@ def get_plain_list(tree): # noqa
                 if children == ADDED:
                     lines.append(f"{node}.{item['key']}")
                     if type(item['value']) is dict:
-                        lines.append('[complex value]')
+                        lines.append(COMPLEX)
                     else:
                         lines.append(item['value'])
-                    lines.append('added')
+                    lines.append(ADDED)
                 if children == REMOVED:
                     lines.append(f"{node}.{item['key']}")
-                    lines.append('removed')
+                    lines.append(REMOVED)
                 if children == CHANGED:
                     lines.append(f"{node}.{item['key']}")
                     if type(item['old_value']) is dict:
-                        lines.append('[complex value]')
+                        lines.append(COMPLEX)
                         lines.append(item['new_value'])
                     elif type(item['new_value']) is dict:
                         lines.append(item['old_value'])
-                        lines.append('[complex value]')
+                        lines.append(COMPLEX)
                     else:
                         lines.append(item['old_value'])
                         lines.append(item['new_value'])
-                    lines.append('updated')
+                    lines.append(CHANGED)
         return lines
     return walk(tree)
+
+
+def convert_to_str(value):
+    if value is True:
+        value = 'true'
+    if value is False:
+        value = 'false'
+    if value is None:
+        value = 'null'
+    return value
 
 
 def get_plain_format(lines): # noqa
     res_lines = []
     for elem in lines:
-        if elem == 'added':
-            if lines[1] is True:
-                lines[1] = 'true'
-            if lines[1] is False:
-                lines[1] = 'false'
-            if lines[1] is None:
-                lines[1] = 'null'
-            if lines[1] == '[complex value]' or\
+        if elem == ADDED:
+            lines[1] = convert_to_str(lines[1])
+            if lines[1] == COMPLEX or\
                lines[1] == 'true' or\
                lines[1] == 'false' or\
                lines[1] == 'null' or\
@@ -70,34 +76,24 @@ def get_plain_format(lines): # noqa
             else:
                 res_lines.append(f"Property '{lines[0][1:]}' was added with value: '{lines[1]}'") # noqa
             lines = lines[3:]
-        if elem == 'removed':
+        if elem == REMOVED:
             res_lines.append(f"Property '{lines[0][1:]}' was removed")
             lines = lines[2:]
-        if elem == 'updated':
-            if lines[1] is True:
-                lines[1] = 'true'
-            if lines[1] is False:
-                lines[1] = 'false'
-            if lines[1] is None:
-                lines[1] = 'null'
-            if lines[2] is True:
-                lines[2] = 'true'
-            if lines[2] is False:
-                lines[2] = 'false'
-            if lines[2] is None:
-                lines[2] = 'null'
+        if elem == CHANGED:
+            lines[1] = convert_to_str(lines[1])
+            lines[2] = convert_to_str(lines[2])
             if (lines[1] == 'true' or lines[1] == 'false' or lines[1] == 'null'
-                or lines[1] == '[complex value]' or type(lines[1]) is int)\
+                or lines[1] == COMPLEX or type(lines[1]) is int)\
                 and (lines[2] == 'true' or lines[2] == 'false'
-                     or lines[2] == 'null' or lines[2] == '[complex value]'
+                     or lines[2] == 'null' or lines[2] == COMPLEX
                      or type(lines[2]) is int):
                 res_lines.append(f"Property '{lines[0][1:]}' was updated. From {lines[1]} to {lines[2]}") # noqa
             elif (lines[1] == 'true' or lines[1] == 'false'
                   or lines[1] == 'null'
-                  or lines[1] == '[complex value]' or type(lines[1]) is int):
+                  or lines[1] == COMPLEX or type(lines[1]) is int):
                 res_lines.append(f"Property '{lines[0][1:]}' was updated. From {lines[1]} to '{lines[2]}'") # noqa
             elif (lines[2] == 'true' or lines[2] == 'false'
-                  or lines[2] == 'null' or lines[2] == '[complex value]'
+                  or lines[2] == 'null' or lines[2] == COMPLEX
                   or type(lines[2]) is int):
                 res_lines.append(f"Property '{lines[0][1:]}' was updated. From '{lines[1]}' to {lines[2]}") # noqa
             else:
